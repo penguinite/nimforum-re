bddd30ccb660b14a2d66aea52c79687e5f1e7c36
penguinite <penguinite@tuta.io>
2024-01-13 20:52:22 +0100

    feat: Implement parseUrlQuery as a wrapper over uri.decodeQuery
    Signed-off-by: penguinite <penguinite@tuta.io>

--- src/frontend/karaxutils.nim
+++ src/frontend/karaxutils.nim
@@ -110,20 +110,8 @@ when defined(js):
   proc renderPostUrl*(threadId, postId: int): string =
     makeUri(fmt"/t/{threadId}#{postId}")
 
-  proc parseUrlQuery*(query: string, result: var Table[string, string])
-    {.deprecated: "use stdlib".} =
-    ## Based on copy from Jester. Use stdlib when
-    ## https://github.com/nim-lang/Nim/pull/7761 is merged.
-    var i = 0
-    i = query.skip("?")
-    while i < query.len()-1:
-      var key = ""
-      var val = ""
-      i += query.parseUntil(key, '=', i)
-      if query[i] != '=':
-        raise newException(ValueError, "Expected '=' at " & $i &
-                           " but got: " & $query[i])
-      inc(i) # Skip =
-      i += query.parseUntil(val, '&', i)
-      inc(i) # Skip &
-      result[$decodeUri(key)] = $decodeUri(val)
+  proc parseUrlQuery*(query: string, result: var Table[string, string]) =
+    ## Wrapper over uri.decodeQuery, Please do not use this for new code.
+    ## This only exists because I could not understand the existing code that depended on it.
+    for key, value in decodeQuery(query):
+      result[key] = value
